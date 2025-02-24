91280c7bb948611f872c12d1fe1a12f16acf0ed9
BR4DKILLER <92213007+BR4DKILLER@users.noreply.github.com>
2025-02-24 14:05:57 +0000

    Moving Files

---
+++ Backend/v1/Register-Code.php
@@ -0,0 +1,13 @@
+<?php 
+    include('Core.php');
+
+    if(isset($_GET['Venue'])){
+        Add_Code_For_Venue($_GET['Venue']);
+    } else {
+        Array_To_JSON(array(
+            "Success" => FALSE,
+            "Message" => "Failed to Register Code - Missing URL Parameters",
+            "Venue" => NULL
+        ));
+    }
+?>
