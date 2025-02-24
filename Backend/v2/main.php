aca0006eca4c39f050db9643df9a8d366604ff2d
BR4DKILLER <92213007+BR4DKILLER@users.noreply.github.com>
2025-02-24 14:14:57 +0000

    Update main.php
    First Version - main.php 
    [REWRITE OF V1 IN PROGRESS]

--- Backend/v2/main.php
+++ Backend/v2/main.php
@@ -1,1 +1,68 @@
+<?php
 
+    /*                        \*
+       LOCAL FUNCTION SECTION
+    */                        \*
+
+    // Establish Connection to MYSQL Database.
+    function Connect_To_Database(){
+        $mysqli = mysqli_connect("localhost", "2378946", "kr8n8s", "db2378946");
+        if ($mysqli -> connect_errno) {
+            echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
+            exit();
+        }
+        return $mysqli;
+    }
+
+    // Query Generation Function (Query [STRING], Args [ARRAY...])
+    function Generate_Query($Query, ...$Args){
+        $mysqli = Connect_To_Database();
+
+        $prepare = $mysqli->prepare($Query);
+
+        $bind_string = '';
+        $bind_args = [];
+
+        foreach($Args as $Array){
+            $bind_string .= $Array[0];
+            $bind_args[] = $Array[1];
+        }
+
+        $bind = $prepare->bind_param($bind_string, ...$bind_args);
+
+        $execute = $prepare->execute();
+        $result = $prepare->get_result();
+
+        $mysqli->close();
+        return [$execute, $result];
+    }
+
+    // JSON / API Response Generation Function (Success [TRUE/FALSE], Message [STRING], Data [...])
+    function Generate_ResponseJSON($Success, $Message, $Data){
+        echo(json_encode([
+            "Success" => $Success,
+            "Message" => $Message,
+            "Data" => $Data
+        ]));
+    }
+
+    // Code Generation Function (6 characters, lowercase)
+    function Generate_Code(){
+        // blank variable, this will be used to store the code
+        $code = '';
+
+        // Generate 6 random characters
+        for($i = 0; $i < 6; $i++){
+            $code = ($code . chr(random_int(97, 122)));
+        }
+
+        // Return Generated Code
+        return $code;
+    }
+
+    /*                         \*
+       PUBLIC FUNCTION SECTION
+    */                         \*
+      
+    echo "Version 2 - INDEV";
+?>
