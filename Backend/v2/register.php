6936c20aff41151cf2a29227cd83467d5ede6977
BR4DKILLER <92213007+BR4DKILLER@users.noreply.github.com>
2025-02-25 17:51:53 +0000

    Create register.php

---
+++ Backend/v2/register.php
@@ -0,0 +1,19 @@
+<?php
+    $method = $_GET;
+    include('main.php');
+    
+    // check if username and password set - > continue script, otherwise - > return invalid param response
+    if(isset($method['username']) and isset($method['password'])){
+
+        // check for secret user and password - > create staff account, otherwise - > create user account
+        if(isset($method['secretu']) and isset($method('secretp'))){
+            Register_Account_Staff($method['username'], $method['password'], $method['secretu'], $method('secretp'));
+        } else {
+            Register_Account_User($method['username'], $method['password']);
+        }
+
+    } else {
+        Generate_ResponseJSON(FALSE, 'ERROR - Invalid parameters', null);
+        die();
+    }
+?>
