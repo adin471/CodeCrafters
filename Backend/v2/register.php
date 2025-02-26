703d9888eee5fac24dc3c6f5e3ac94cfe672874d
BR4DKILLER <92213007+BR4DKILLER@users.noreply.github.com>
2025-02-26 07:56:13 +0000

    Update register.php
    Updated to lastest version

--- Backend/v2/register.php
+++ Backend/v2/register.php
@@ -1,7 +1,11 @@
 <?php
+    // set $method as one of the below specified.
+    // 1 - > $_GET for testing purposes 
+    // 2 - > $_POST for live version.
+
     $method = $_GET;
     include('main.php');
-    
+
     // check if username and password set - > continue script, otherwise - > return invalid param response
     if(isset($method['username']) and isset($method['password'])){
 
