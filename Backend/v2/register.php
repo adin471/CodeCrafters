6e61697d118a9b7e260009234f029868b28963e9
BR4DKILLER <92213007+BR4DKILLER@users.noreply.github.com>
2025-02-26 06:28:07 +0000

    Update register.php
    fixed $method('secretp') to be $method['secretp']

--- Backend/v2/register.php
+++ Backend/v2/register.php
@@ -6,8 +6,8 @@
     if(isset($method['username']) and isset($method['password'])){
 
         // check for secret user and password - > create staff account, otherwise - > create user account
-        if(isset($method['secretu']) and isset($method('secretp'))){
-            Register_Account_Staff($method['username'], $method['password'], $method['secretu'], $method('secretp'));
+        if(isset($method['secretu']) and isset($method['secretp'])){
+            Register_Account_Staff($method['username'], $method['password'], $method['secretu'], $method['secretp']);
         } else {
             Register_Account_User($method['username'], $method['password']);
         }
