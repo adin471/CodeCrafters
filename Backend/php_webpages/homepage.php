73e6cbebe0a153afb7780c1372a05b1c5ad931d1
bsdhesi <186108470+bsdhesi@users.noreply.github.com>
2025-03-16 02:18:19 +0000

    added barebone websites
    no functionality in webpages for now, to be added later

---
+++ Backend/php_webpages/homepage.php
@@ -0,0 +1,18 @@
+<?php
+require_once "../../../vendor/autoload.php";
+
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
+
+try {
+    $loader = new FilesystemLoader('../templates');
+    $twig = new Environment($loader);
+
+    // Render the homepage template
+    echo $twig->render('homepage.html.twig');
+} catch (Exception $e) {
+    // Handle any Twig errors
+    echo 'Error loading template: ' . $e->getMessage();
+}
+?>
+
