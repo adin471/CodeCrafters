287ab070cb16fe404ad44e2eea1569daafacae31
bsdhesi <186108470+bsdhesi@users.noreply.github.com>
2025-03-21 04:10:22 +0000

    update to login/register page
    added a check to redirect user to homepage if user is already logged in

--- Backend/php_webpages/attendance.php
+++ Backend/php_webpages/attendance.php
@@ -1,5 +1,13 @@
 <?php
-require_once "../../../vendor/autoload.php";
+session_start();
+
+// Check if user is already logged in
+if (isset($_SESSION['username'])) {
+    header('Location: homepage.php'); // Redirect to another page if logged in
+    exit();
+}
+
+require_once "../../vendor/autoload.php";
 
 use Twig\Environment;
 use Twig\Loader\FilesystemLoader;
@@ -8,11 +16,10 @@ try {
     $loader = new FilesystemLoader('../templates');
     $twig = new Environment($loader);
 
-    // Render the homepage template
+    // Render the attendance template
     echo $twig->render('attendance.html.twig');
 } catch (Exception $e) {
     // Handle any Twig errors
     echo 'Error loading template: ' . $e->getMessage();
 }
 ?>
-
